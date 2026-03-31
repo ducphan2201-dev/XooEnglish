47a351da1fcb90613f1eeca98e99828cce0a756e
ducphan2201-dev <vyd.info@gmail.com>
2026-03-31 19:05:11 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,5 +6,5 @@
 const CONFIG = {
     // Để trống chuỗi này ("", hoặc null) hệ thống sẽ chạy ở chế độ DEMO Ảo
     // Nhập URL web app Google Apps Script vào đây để kết nối CSDL Thật
-    API_URL: ""
+    API_URL: "https://script.google.com/macros/s/AKfycbyXz4OvlGOx-EEPT6JqkO71yZsk58u9DOJVGd2bcH9c51omIEZiLtV-ehxUU9n1R49u/exec"
 };
